4fceafdb1efcbd017dc5f893149f52311433052b
zeke-xcviii <ezvipz20@gmail.com>
2021-04-05 19:05:00 +0800

    added config

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    API_KEY: '42aec3b9-b2d3-4bc8-9808-0eaaf63a3b65',
+    
+    cat_img: 'https://api.thecatapi.com/v1/images/search?mime_types=gif',
+
+    cat_facts: 'https://my-json-server.typicode.com/zeke-xcviii/mock/facts'
+};
+
+ 
