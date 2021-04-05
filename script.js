b39a80b5881bb98736e79588f5b7bc4f6803cd4b
zeke-xcviii <ezvipz20@gmail.com>
2021-04-05 18:52:55 +0800

    fixed some bugs

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-/*Learning to hide variables using .gitignore */
-
-// From: https://docs.thecatapi.com
 const url_cat_imgs = config.cat_img;
-// A fake REST APi made by Typicode
+
 const url_cat_facts = config.cat_facts;
 
 const api_key = config.API_KEY;
 
-/* get cat facts
-   datas from:  https://github.com/vadimdemedes/cat-facts/blob/master/cat-facts.json
-*/
 async function facts() {
   const response = await fetch(url_cat_facts);
   const data = await response.json();
   document.getElementById('facts').innerHTML = data[Math.floor(Math.random()*data.length)];
 }
 
-// get cat images from thecatapi.com
 async function cats() {
   const response = await fetch(url_cat_imgs,
   {
@@ -35,3 +28,4 @@ facts().catch((error) => {
 cats().catch((error) => {
     alert(error);
 });
+
