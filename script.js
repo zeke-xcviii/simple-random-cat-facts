a1a310d51ffaf34e594a2610b6f360e95f9f937b
zeke-xcviii <ezvipz20@gmail.com>
2021-04-06 21:04:47 +0800

    added animation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const api_key = config.API_KEY;
 async function facts() {
   const response = await fetch(url_cat_facts);
   const data = await response.json();
-  document.getElementById('facts').innerHTML = data[Math.floor(Math.random()*data.length)];
+  return data[Math.floor(Math.random()*data.length)];
 }
 
 async function cats() {
@@ -30,3 +30,80 @@ cats().catch((error) => {
     alert(error);
 });
 
+var Animation = function(el) {
+    'use strict';
+    var a = this;
+    
+    a.init = async function() {
+        a.el = el;
+        a.letters = "abcdefghijklmnopqrstuvwxyz";
+        a.current_length = 0;
+        a.fadeBuffer = false;
+        a.message = await facts();
+        setTimeout(a.animate, 100);
+    };
+    
+    a.randomizeString = function(length) {
+        
+        let random_text = '';
+        
+        while(random_text.length < length) {
+            random_text += a.letters.charAt(Math.floor(Math.random() * a.letters.length));
+        }
+        
+        return random_text;
+    };
+    
+    a.animate = function() {
+        if(a.current_length < a.message.length) {
+            a.current_length += 2;
+            if (a.current_length > a.message.length) {
+                a.current_length = a.message.length;
+            }
+            
+            let message = a.randomizeString(a.current_length);
+            a.el.innerHTML = message;
+            
+            setTimeout(a.animate, 30);
+        } else {
+            setTimeout(a.animateFadeBuffer, 30);
+        }
+    };
+    
+    a.animateFadeBuffer = function() {
+        
+        if(a.fadeBuffer === false) {
+            a.fadeBuffer = [];
+            for(let i = 0; i < a.message.length; i++) {
+                a.fadeBuffer.push({
+                    c: (Math.floor(Math.random() * Math.floor(a.message.length / 4)) + 1),
+                    l: a.message.charAt(i)
+                });
+            }
+        }
+        
+        let do_cycles = false;
+        let message = '';
+        
+        for(let i = 0; i < a.fadeBuffer.length; i++) {
+            let fader = a.fadeBuffer[i];
+            if(fader.c > 0) {
+                do_cycles = true;
+                fader.c--;
+                message += a.letters.charAt(Math.floor(Math.random() * a.message.length));
+            } else {
+                message += fader.l;
+            }
+        }
+        
+        a.el.innerHTML = message;
+        
+        if(do_cycles === true) {
+            setTimeout(a.animateFadeBuffer, 30);
+        }
+    };
+    
+    a.init();
+};
+
+var anime = new Animation(document.getElementById('facts'));
